4436a33184aedd3e6e9992feaa5314b258441d41
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2020-07-22 19:54:44 -0300

    User entity and resource

---
+++ src/main/java/com/vallosstudio/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.vallosstudio.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vallosstudio.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Joao da Silva", "joao@gmail.com", "999999", "999999");
+		return ResponseEntity.ok().body(u);
+	}
+}
