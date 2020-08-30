57d81da36070870dbd920f22cbd8d4b474ee51cd
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2020-08-30 19:51:23 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/vallosstudio/course/config/TestConfig.java
+++ src/main/java/com/vallosstudio/course/config/TestConfig.java
@@ -47,16 +47,27 @@ public class TestConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "Maria da Silva", "maria@gmail.com", "999999", "99999");
 		User u2 = new User(null, "Carlos Maurício", "carlos@gmail.com", "999999", "99999");
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 	}
 
 }
