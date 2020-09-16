6999fd25e5fbb5723093e4e9aaea6725de12c15d
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2020-09-16 18:33:03 -0300

    Exception handling - delete

--- src/main/java/com/vallosstudio/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/vallosstudio/course/resources/exceptions/ResourceExceptionHandler.java
@@ -3,12 +3,14 @@ package com.vallosstudio.course.resources.exceptions;
 import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.vallosstudio.course.services.exceptions.DatabaseException;
 import com.vallosstudio.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -18,6 +20,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> constraintViolation(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
